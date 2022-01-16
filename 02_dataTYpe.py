c85b5572930a71f23c8f30ce42c5282cca06e166
saidmtanzania <saidmtanzania@gmail.com>
2022-01-16 13:40:31 +0300

    first commit

---
+++ 02_dataTYpe.py
@@ -0,0 +1,26 @@
+####Learning Data Types in python
+#String
+#can use double and single quotation"",''
+nick_name = "saidmtanzania"
+city = "Dar es salaam"
+#Integer
+age = 35
+#float
+weight = 91.21
+productivity = 0.97
+#boolean
+#it has True and False
+isHeok = True
+isSHefine = False
+#list
+weight = [ 52,56,78,23,90.1,True,False,0.134 ]
+#dictionary
+book = {
+     "key": "values",
+     "title": "how to be awesome",
+     "author": "John Doe",
+     "year": 2019,
+     "Published": "23-9-2021"
+ }
+print(book["author"])
+print(book["title"])

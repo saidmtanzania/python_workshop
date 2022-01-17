d5b0158d2c96f3280d5f39b509b2daace253cf1f
saidmtanzania <saidmtanzania@gmail.com>
2022-01-17 21:52:28 +0300

    inital changes

--- 03.0_conditional.py
+++ 03.0_conditional.py
@@ -3,3 +3,15 @@ Decision-making in a programming language is automated
 using conditional statements, in which Python evaluates
 the code to see if it meets the specified conditions.
 """
+isPasswdValid = True
+isAdmin = True
+
+#When u are using if elseif only the single statement 
+# that meet condition can execute 
+if isPasswdValid:
+    print("password is Valid")
+elif isAdmin:
+    print("password is valid, but not admin")
+else:
+    print("password is valid, but not Admin")
+#

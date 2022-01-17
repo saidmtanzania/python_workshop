7ee9eb1bc752cd5487e74e936612b3a94a3a1b4b
saidmtanzania <saidmtanzania@gmail.com>
2022-01-17 21:18:02 +0300

    initial changes

---
+++ 01.0_variable.py
@@ -0,0 +1,22 @@
+#### storage system
+# store any type of number 
+x = 2
+pi = 3.1415
+age = 21
+## store name
+name = "salim"
+first_name = "Mohammed"
+## this hash means commenting "#"
+## rules of variable:
+        #they shouldnt start with number 
+        #must start with a letter or the underscore character.
+        #can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+        #case-sensitive (name, Name and NAME are three different variables).
+        # The reserved words(keywords) cannot be used naming the variable. 
+_ch = "collumn"
+print(_ch)
+##printing variables 
+print(first_name)
+print(x)
+print(pi)
+print(age)

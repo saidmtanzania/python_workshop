328881f2685d182aa1e4edbb6d3100fc141eaeb5
saidmtanzania <saidmtanzania@gmail.com>
2022-01-17 18:29:56 +0300

    initial changes

--- 02.3_List.py
+++ 02.3_List.py
@@ -32,3 +32,20 @@ temp1, temp2, temp3  = temperature
 print(f"The value of first temp1 are: {temp1}")
 print(f"The value of first temp2 are: {temp2}")
 print(f"The value of first temp3 are: {temp3}")
+#List also called Array
+#List is the collection of similar items.
+tempav1_temperature = [234,55,456.0,3451,34,901]
+
+#printing the length of the tempav1_temperature
+print(f"The lenght of temperature value is :{len(tempav1_temperature)}")
+
+#printing value in the list
+#[234,55,456.0,3451,34,901]
+#  0  1   2     3   4   5
+
+v1_temperature = tempav1_temperature[0]
+print(f"the value of first temperature is: {v1_temperature}")
+v2_temperature = tempav1_temperature[1]
+print(f"the value of second temperature is: {v2_temperature}")
+v3_temperature = tempav1_temperature[2]
+print(f"the value of third temperature is: {v3_temperature}")
